02f646f791ab2b892280bfba8c7e23ded06e4e27
olawale afuye <walosha@yahoo.com>
2022-09-18 23:07:07 +0100

    fixed issues

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -2,13 +2,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.m?(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -2,7 +2,6 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const PackageJSON = require("../package.json");
-const DOMAIN = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: "production",
