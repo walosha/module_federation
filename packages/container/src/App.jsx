e4dac25ce5f9a4e502a47b3a66d6e11868b194b6
olawale afuye <walosha@yahoo.com>
2022-09-18 21:16:41 +0100

    added distribution ID

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -4,7 +4,7 @@ import MarkingApp from "./components/marketing";
 export default function App() {
   return (
     <>
-      <h1>Container Applications !!!</h1>
+      <h1>Container Applications</h1>
       <hr />
       <MarkingApp />
     </>
