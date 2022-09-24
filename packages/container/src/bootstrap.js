21692f2f0c8f1cbfad04c859d5bd859aad3ddd20
olawale afuye <walosha@yahoo.com>
2022-09-24 23:47:05 +0100

    fixed issues with mounting container

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,16 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
-};
-
-const ContainerRoot = document.querySelector("#root");
-
-if (process.env.NODE_ENV === "development") {
-  if (ContainerRoot) {
-    mount(ContainerRoot);
-  }
-}
-
-export { mount };
+ReactDOM.render(<App />, document.querySelector("#root"));
