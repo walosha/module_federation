12ea588cd3b86418dfd39829fd482cf2e715f127
olawale afuye <walosha@yahoo.com>
2022-09-25 10:00:24 +0100

    class name generator added for prod

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import Header from "./components/header";
 import MarkingApp from "./components/marketing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "marketing",
+});
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarkingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarkingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import LandinPage from "./components/Landing";
 import PricingPage from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "marketing",
+});
+
 export default function App() {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/pricing" component={PricingPage} />
